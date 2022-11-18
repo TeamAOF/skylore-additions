d4d1b3164186ea37f34c97096ddb4a3591e636a1
_Robertas <66907345+RobertasJ@users.noreply.github.com>
2022-11-18 22:59:27 +0100

    skylore scripts
    includes example configs in the scripts itself

---
+++ sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+source ./config.sh
+# example config
+# #!/bin/bash
+# owner="TeamAOF" # creator of the git repository.
+# repo="skylore" # name of the git repository.
+# branch="indev" # branch of the git repository. for examle main or master.
+
+repoExists=$(git rev-parse --is-inside-work-tree)
+url="https://github.com/${owner}/${repo}.git"
+
+# code
+git config pull.rebase false
+if [ "$repoExists" ]
+then
+  git reset --hard
+  git pull
+  git switch "$branch"
+  java -jar InstanceSync.jar
+else
+  git clone "$url" modpack
+  cp -a modpack/. .
+  rm -r -f modpack
+  git reset --hard
+  git pull
+  git checkout "$branch"
+  java -jar InstanceSync.jar
+fi
+if [ -f "server.lock" ]
+then
+bash server.lock
+fi
